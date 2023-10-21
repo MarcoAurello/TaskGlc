4ed13f736d29547236342ecf335b25a8f57a5f4b
Diego Alisson Monteiro <diegoalisson@pe.senac.br>
2023-10-21 08:55:59 -0300

    desabilitando carregarEmails | pages/home.js

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -163,7 +163,7 @@ const Home = (props) => {
     // carregarMinhasAtividades();
     // carregarSolicitacaoAtividades();
     // carregarAtividadesDoSetor()
-    carregarEmails();
+    // carregarEmails();
 
     if (todosEmails && logged) {
       let encontrado = false;
