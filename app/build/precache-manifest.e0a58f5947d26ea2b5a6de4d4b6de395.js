50588c2811f092028e5e96b8db0f9421b92380a6
marco <marco.nunes.senac@gmail.com>
2023-10-23 15:50:33 -0300

    filtro recebidos1

---
+++ app/build/precache-manifest.e0a58f5947d26ea2b5a6de4d4b6de395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecbbde2d3d10c2b369805bba7a48418",
+    "url": "/taskmanager/index.html"
+  },
+  {
+    "revision": "8ea2d3bad93e9949ca46",
+    "url": "/taskmanager/static/js/2.14a14fe0.chunk.js"
+  },
+  {
+    "revision": "b292148615d2cd6591412b789f7ea08c",
+    "url": "/taskmanager/static/js/2.14a14fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5ff91470520f5db4a9",
+    "url": "/taskmanager/static/js/main.90cbe430.chunk.js"
+  },
+  {
+    "revision": "a76c36cb040031857a49",
+    "url": "/taskmanager/static/js/runtime-main.d5841d58.js"
+  },
+  {
+    "revision": "b0ed9ac9e620d778675277b32fb0a02d",
+    "url": "/taskmanager/static/media/bg-image.b0ed9ac9.jpeg"
+  }
+]);
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-console.log("teste");
 import App from "./app";
+// console.log("teste");
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
