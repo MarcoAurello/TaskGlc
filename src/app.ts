477d150023542165c9c189fada210627e80d1f6d
Diego Alisson Monteiro <diegoalisson@pe.senac.br>
2023-10-26 14:43:59 -0300

    uncaughtException

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
+/* eslint-disable n/handle-callback-err */
+/* eslint-disable no-trailing-spaces */
 import app from './server'
+import process from 'process'
+const path = require('path')
+const fs = require('fs')
 
-function run () {
-  try {
-    const PORT = 3354
-    const HOST = '0.0.0.0'
+process.on('uncaughtException', (err, origin) => {
+  const date = new Date()
+  const fileName = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.txt`
 
-    app.listen(PORT, HOST, () => {
-      console.log(`The server is running on port ${PORT}`)
-    })
-  } catch (err) {
-    run()
-    console.log(err)
-  }
-}
+  fs.writeFile(
+    path.join(__dirname, 'logs', fileName),
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`,
+    (err) => {
+      if (err) {
+        console.log(err)
+      }
+    }
+  )
+})
 
-run()
+const PORT = 3354
+const HOST = '0.0.0.0'
+
+app.listen(PORT, HOST, () => {
+  console.log(`The server is running on port ${PORT}`)
+})
