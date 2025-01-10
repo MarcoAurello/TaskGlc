1d27f7f0634fb73a4fe8ce4768e08dc9d2c35775
marco <marco.nunes.senac@gmail.com>
2025-01-10 13:47:43 -0300

    alter

---
+++ app/build/precache-manifest.31166268de36b65e04c5707d4fefa051.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706f6b34bbfe6655b2b764d56e57621c",
+    "url": "/taskmanagerglc/index.html"
+  },
+  {
+    "revision": "cd44615ba4fd0943305c",
+    "url": "/taskmanagerglc/static/css/main.7bfdbf3e.chunk.css"
+  },
+  {
+    "revision": "c4eae29459048535516d",
+    "url": "/taskmanagerglc/static/js/2.89edf802.chunk.js"
+  },
+  {
+    "revision": "fa6d006ff944d0dca4cd1d5124d2a964",
+    "url": "/taskmanagerglc/static/js/2.89edf802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd44615ba4fd0943305c",
+    "url": "/taskmanagerglc/static/js/main.665c870a.chunk.js"
+  },
+  {
+    "revision": "17e6d21b9ca0caefd5e1",
+    "url": "/taskmanagerglc/static/js/runtime-main.7001a50a.js"
+  },
+  {
+    "revision": "92e5e1aff734fe5c60a6f230131e1ad9",
+    "url": "/taskmanagerglc/static/media/ane.92e5e1af.png"
+  },
+  {
+    "revision": "e507e3af5f7da2c06765cbcd2a29d0b0",
+    "url": "/taskmanagerglc/static/media/anexo.e507e3af.jpg"
+  },
+  {
+    "revision": "0ce5de5ea20ca3c686800b7e56f1e565",
+    "url": "/taskmanagerglc/static/media/bg-image.0ce5de5e.jpg"
+  },
+  {
+    "revision": "e9adf1b35b8c70765ab1c23e02ebefa9",
+    "url": "/taskmanagerglc/static/media/coment.e9adf1b3.png"
+  },
+  {
+    "revision": "97c3486a4e5c2ee3302c942eaa1524c2",
+    "url": "/taskmanagerglc/static/media/plan.97c3486a.png"
+  }
+]);
